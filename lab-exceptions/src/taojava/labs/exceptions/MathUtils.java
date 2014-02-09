99f99a5f08893ced1cf59e05c0e6606d4bce3b90
Khoa Nguyen <nguyenkh@cs.grinnell.edu>
2014-02-09 15:25:43 -0600

    Exception lab submission

---
+++ lab-exceptions/src/taojava/labs/exceptions/MathUtils.java
@@ -0,0 +1,12 @@
+package taojava.labs.exceptions;
+
+/**
+ * A variety of utilities for doing Math.
+ * 
+ * @author Samuel A. Rebelsky
+ * @author Your Name Here
+ */
+public class MathUtils
+{
+
+} // MathUtils
